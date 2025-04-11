a034661e5ba158ec8146f7cb4d900bc6bbed1ea1
Nikita Savin <klaster26@mail.ru>
2025-04-11 18:31:55 +0300

    feat: реализовать дополнительные модели

--- rewards/models.py
+++ rewards/models.py
@@ -14,3 +14,50 @@ class User(AbstractUser):
     coins = models.IntegerField(
         default=0, blank=True, verbose_name="Количество монет у пользователя"
     )
+
+    def __str__(self):
+        return self.username
+
+
+class ScheduledReward(models.Model):
+    user = models.ForeignKey(
+        to=get_user_model(),  # Несмотря на то, что модель пользователя
+                              # определена у нас тут же, не стоит наступать
+                              # на грабли и ссылаться непосредственно на объект
+                              # модели. Django предоставляет невероятно удобный
+                              # интерфейс для того, чтобы в любом месте кода
+                              # получать текущий объект модели пользователя,
+                              # не будучи привязанным к одной конкретной модели
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name="Пользователь, запросивший награду",
+    )
+    amount = models.IntegerField(
+        default=0, blank=True, verbose_name="Количество монет к начислению"
+    )
+    execute_at = models.DateTimeField(
+        verbose_name="Планируемое время начисления награды"
+    )
+
+    def __str__(self):
+        return (f"Награда пользователю {self.user} в размере {self.amount} "
+                f"монет, запланированная к выдаче {self.execute_at}")
+
+
+class RewardLog(models.Model):
+    user = models.ForeignKey(
+        to=get_user_model(),
+        null=True,
+        on_delete=models.SET_NULL,
+        verbose_name="Пользователь, получивший награду",
+    )
+    amount = models.IntegerField(
+        default=0, blank=True, verbose_name="Количество начисленных монет"
+    )
+    given_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Фактическое время начисления награды"
+    )
+
+    def __str__(self):
+        return (f"Награда пользователю {self.user} в размере {self.amount} "
+                f"монет, выданная {self.given_at}")
