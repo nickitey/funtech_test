ecd9a2e0dbc07f0c58c59a99339e8118524fbe86
Nikita Savin <klaster26@mail.ru>
2025-04-13 21:15:09 +0300

    test: реализовать еще тесты для views

--- tests/views/conftest.py
+++ tests/views/conftest.py
@@ -1,5 +1,7 @@
 import json
 
+from django.conf import settings
+from redis import Redis
 import pytest
 from pytest_django.fixtures import rf
 
@@ -22,3 +24,24 @@ def access_token(user_for_tests, user_data, rf):
     response = token_view(request)
     response_body = json.loads(response.render().content.decode())
     return response_body["access"]
+
+
+@pytest.fixture
+def redis_client():
+    redis_client = Redis(
+            host=settings.REDIS_HOST,
+            port=settings.REDIS_PORT,
+            username=settings.REDIS_USER,
+            password=settings.REDIS_PASSWORD
+    )
+    try:
+        # Мы не ожидаем каких-то ошибок, которые приведут к аварийному завершению
+        # тестов, но для большего контроля в любом случае, даже если возникнет
+        # непредвиденное исключение, мы в finally закроем соединение.
+        yield redis_client
+        # Очистка кэша после теста как гарантия идемпотентности стартовых условий.
+        redis_client.flushall()
+    finally:
+        # По большому счету, после остановки контейнера при завершении тестов
+        # соединение и так будет закрыто. Но "явное лучше, чем неявное" (С).
+        redis_client.close()
--- tests/views/test_views.py
+++ tests/views/test_views.py
@@ -1,9 +1,10 @@
 import json
+import pickle
+from datetime import datetime, timezone
 
 import pytest
-from django.core.cache import cache as django_cache
-from redis import Redis
 
+from rewards.models import ScheduledReward
 from rewards.views import (UserInformationView, UserRegistrationView,
                            UserRewardRequestView, UserRewardsListView)
 
@@ -36,3 +37,79 @@ class TestViews:
         }
 
         assert response_body == ethalon
+
+    def test_user_awards(self, rf, user_for_tests, access_token):
+        [
+            ScheduledReward.objects.create(
+                user=user_for_tests,
+                amount=amount,
+                execute_at=datetime.now(tz=timezone.utc),
+            )
+            for amount in (5, 7, 9)
+        ]
+
+        request = rf.get(
+            "api/rewards/", headers={"Authorization": f"Bearer {access_token}"}
+        )
+        awards_view = UserRewardsListView.as_view()
+        response = awards_view(request)
+        response_body = json.loads(response.render().content.decode())
+
+        assert len(response_body["rewards"]) == 3
+        assert (
+            sum(reward["amount"] for reward in response_body["rewards"]) == 21
+        )
+
+    def test_user_award_request(self, rf, user_for_tests, access_token):
+        # Убедимся, что до начала теста у пользователя нет монет.
+        assert user_for_tests.coins == 0
+        request = rf.post(
+            "api/rewards/request/",
+            headers={"Authorization": f"Bearer {access_token}"},
+        )
+
+        award_request_view = UserRewardRequestView.as_view()
+        response = award_request_view(request)
+        response_body = json.loads(response.render().content.decode())
+        ethalon = {
+            "user": "testuser",
+            "reward request status": "Success",
+            "description": "Награда запрошена и будет зачислена в течение пяти минут.",
+        }
+        assert response_body == ethalon
+
+        user_for_tests.refresh_from_db()
+        # После выполнения запроса у пользователя становится 5 монет.
+        assert user_for_tests.coins == 5
+
+    def test_forbid_second_user_award_request_(
+        self, rf, user_for_tests, access_token, redis_client
+    ):
+        assert user_for_tests.coins == 0
+        request = rf.post(
+            "api/rewards/request/",
+            headers={"Authorization": f"Bearer {access_token}"},
+        )
+
+        award_request_view = UserRewardRequestView.as_view()
+        award_request_view(request)
+        response = award_request_view(request)
+        response_body = json.loads(response.render().content.decode())
+
+        ethalon = {
+            "user": "testuser",
+            "reward request status": "Not Allowed",
+            "description": "Вы уже запрашивали награду в течение последних суток.",
+        }
+        assert response_body == ethalon
+
+        # Убедимся, что в кэше действительно есть запись, связанная с нашим
+        # пользователем, который запросил награду
+        with redis_client as redis:
+            cache_key = f"rewards:1:user_{user_for_tests.pk}_requested_reward"
+            content = redis.get(cache_key)
+            assert pickle.loads(content) is True
+
+            # Кроме того, убедимся, что срок жизни указанной записи немногим
+            # меньше 86400 секунд (60 сек * 59 мин * 24 ч)
+            assert redis.ttl(cache_key) > 60 * 59 * 24
